5b8fee5cf69d5aae36d2ace5ed10ea10ae6d9a4d
Lee Taesung <leetsxpro@gmail.com>
2022-06-14 18:58:33 +0900

    2022-06-14 Update2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     {/* <React.StrictMode> useEffect()가 두 번 실행 방지*/}
       <App />
     {/* </React.StrictMode> */}
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
